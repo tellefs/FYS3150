645753a1cf4102f2c62f8af7a3ecb42d31d53f79
Tellef Storebakken <tellefstorebakken@gmail.com>
2016-09-08 12:48:23 +0200

    made all arrays, next step calculate u array

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -19,20 +20,23 @@ int main()
 {
     int const n = 10;
     double h = 1/(n+1);
-    double *b_arr = new double[n-1]; //should contain 1's
+    double *b_arr = new double[n]; //should contain 1's
     double *a_arr = new double[n];   //should contain -2's
-    double *c_arr = new double[n-1]; //should contain 1's
+    double *c_arr = new double[n]; //should contain 1's
     double *v_arr = new double[n];
     double *f_arr = new double[n];
+    double *f_tilde = new double[n];
     double *a_tilde = new double[n];
 
     for(int i=0; i<n; i++){
         b_arr[i] = 1;
         a_arr[i] = -2;
         c_arr[i] = 1;
-        a_tilde[i] = a_arr[i] - (c_arr[i-1]*b_arr[i-1])/a_tilde[i-1];
-        cout<<a_tilde[i]<<endl;
+        f_arr[i] = -h*h*100*exp(-10*i*h);
+        a_tilde[i] = a_arr[i] - (b_arr[i-1]*c_arr[i-1])/a_tilde[i-1];
+        f_tilde[i] = f_arr[i] - f_tilde[i]*(c_arr[i-1]/a_arr[i-1]);
     }
+
 
 
     return 0;
