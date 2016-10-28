1cc984c02d34d163b4b99f7f936d5d3f26c87629
Tellef Storebakken <tellefstorebakken@gmail.com>
2016-10-28 11:36:36 +0200

    added project4 and main.cpp

--- Project3/Project3_cpp/solver.cpp
+++ Project3/Project3_cpp/solver.cpp
@@ -12,9 +12,10 @@ Solver::Solver()
 
 void Solver::forwardEulerMethod(mat pos, mat vel, double force, int N, double h){
     //function to integratre numerically using eulers forward method
-    //the matrix pos should contain 3 N-long arrays representing x,y and z directions
-    //same with matrix vel, only with velocity
-    // force is the value of the force, N is array length and h is the steplength
+    //the matrix pos should contain 3 N-long arrays representing x,y and z position
+    //same with matrix vel, only for the velocity
+    // force is the value of the force which needs to be multiplied with the position
+    //N is array length and h is the steplength
 
     for(i=0; i<N; i++){
 
@@ -30,9 +31,10 @@ void Solver::forwardEulerMethod(mat pos, mat vel, double force, int N, double h)
 
 void Solver::verletMethod(mat pos, vec vel, double force_factor, double h, int N){
     //function to integratre numerically using the verlet method
-    //the matrix pos should contain 3 N-long arrays representing x,y and z directions
-    //same with matrix vel, only with velocity
-    // force is the value of the force, N is array length and h is the steplength
+    //the matrix pos should contain 3 N-long arrays representing x,y and z position
+    //same with matrix vel, only for the velocity
+    // force is the value of the force which needs to be multiplied with the position
+    //N is array length and h is the steplength
 
     for(i=1; i<N, i++){
         double r = sqrt(pos(0,i)*pos(0,i) + pos(1,i)*pos(1,i) + pos(2,i)*pos(2,i));
