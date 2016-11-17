e7d933158915ae817f990575fd86ac90f2038da0
Tellef Storebakken <tellefstorebakken@gmail.com>
2016-11-17 12:27:17 +0100

    added files stockMarketModel

---
+++ project5/stockMarketModel/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <armadillo>
+#include <cmath>
+
+
+using namespace std;
+using namespace arma;
+
+
+
+int main()
+{
+
+    int N = 500;
+    int transactions = 1e7;
+    int mccMax = 1e4;
+    int m0 = 1;
+
+    vec m(N, m0);
+
+    for(int mcc=0; mcc<mccMax; mcc++){
+
+
+
+    }
+
+    return 0;
+}
+
